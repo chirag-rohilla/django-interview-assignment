baa8198630c4d012778352b10497cee67762751c
chirag_rohilla <rohillachirag783@gmail.com>
2022-07-09 11:30:33 +0530

    models created

--- library_management_system/app/admin.py
+++ library_management_system/app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+#from django.contrib.sessions.models import Session
+from .models import AddBook, AddMember, IssueBook, ReturnBook, UserExtend
 
 # Register your models here.
+
+#admin.sites.register(UserExtend)
+
+admin.site.register(UserExtend)
+
+class AddBook_admin(admin.ModelAdmin):
+    list_display = ("user","book_name", "book_id", "subject" , "author")
+admin.site.register(AddBook,AddBook_admin)
+
+class IssueBook_admin(admin.ModelAdmin):
+    list_display = ("user", "m_id", "bookid1", "issuedate", "expirydate")
+admin.site.register(IssueBook,IssueBook_admin)
+
+class ReturnBook_admin(admin.ModelAdmin):
+    list_display =("user", "bookid2")
+admin.site.register(ReturnBook, ReturnBook_admin)
+
+class AddMember_admin(admin.ModelAdmin):
+    list_display = ("user", "m_name", "m_id")
+admin.site.register(AddMember,AddMember_admin)
--- library_management_system/app/models.py
+++ library_management_system/app/models.py
@@ -1,3 +1,46 @@
+from xml.dom.minidom import CharacterData
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField as Char 
+from django.db.models.fields import IntegerField as Int 
+from django.db.models.fields import DateField as data
+from datetime import datetime ,timedelta 
 
-# Create your models here.
+
+class UserExtend (models.Model):
+    user = models.OneToOneField(User,default=1,on_delete=models.CASCADE)
+    ph_no= Int()
+    def __str__(self):
+        return self.user.username
+
+class AddBook (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_name = Char(max_length=10)
+    book_id = Char(max_length=10)
+    subject = Char(max_length=10)
+    author = Char(max_length=15)
+    def __str__(self):
+        return str(self.book_name)+" : "+str(self.book_id)
+
+def expiry():
+    return datetime.today + timedelta(days=15)
+
+class IssueBook (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    m_id = Char(max_length=10)
+    bookid1 = Char(max_length=10)
+    issuedate = data(auto_now=True)
+    expirydate = data(default = expiry) 
+    def __str__(self):
+        return self.m_id
+    
+class ReturnBook (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    bookid2 = Char(max_length=10)
+    
+class AddMember(models.Model):
+    user = models.OneToOneField(User,default=0, on_delete=models.CASCADE)
+    m_name = Char(max_length=10)
+    m_id = Char(max_length=10)
+    def __str__(self):
+        return str(self.m_name)+' : '+str(self.m_id)
